464eba955057c0fd61c3604188d384fa8bbd8125
Vimal Bharti <s.vimalbharti@gmail.com>
2025-03-03 12:55:11 +0530

    resonsive

--- pages/index.vue
+++ pages/index.vue
@@ -48,10 +48,10 @@
     <!-- ========== MAIN CONTENT ========== -->
     <main>
       <!-- Hero -->
-      <div class="bg-white object-cover h-[80vh] flex flex-col items-center justify-center bg-[linear-gradient(to_top,rgba(0,0,0,0.8),rgba(255,255,255,0)),url('hero.jpg')] bg-center" 
+      <div class="bg-white object-cover h-[60vh] md:h-[80vh] flex flex-col items-center justify-center bg-[linear-gradient(to_top,rgba(0,0,0,0.8),rgba(255,255,255,0)),url('hero.jpg')] bg-center" 
         id="home"
       >
-        <div class="max-w-[40vw] w-full mx-auto pt-20">
+        <div class="md:max-w-[40vw] px-4 md:px-0 w-full mx-auto pt-20">
           <h1 class="font-semibold text-white text-5xl md:text-6xl text-center">
             <span class="text-cyan-600 font-bold">MeetoU</span> <br/> Transforming ideas into reality
           </h1>
@@ -67,9 +67,9 @@
       <!-- Clients -->
       <div class="relative overflow-hidden bg-white">
 
-        <div class="relative z-10 py-5 w-full bg-gray-100">
+        <div class="relative z-10 md:py-5 w-full bg-gray-100">
           <div class="max-w-5xl  mx-auto shadow-xl rounded-xl border px-4 py-6 bg-white">
-            <div class="grid grid-cols-3 gap-6">
+            <div class="grid md:grid-cols-3 gap-6">
               <div class="flex item-center gap-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"><g fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M19.647 15.536H4.353m15.294 0V8c0-1.886 0-2.828-.586-3.414C18.476 4 17.533 4 15.647 4H8.353c-1.886 0-2.828 0-3.414.586S4.353 6.114 4.353 8v7.536m15.294 0l1.744 1.8l.088.092a2 2 0 0 1 .52 1.284l.001.127c0 .15 0 .224-.004.287a2 2 0 0 1-1.87 1.87a5 5 0 0 1-.287.004H4.161c-.15 0-.224 0-.287-.004a2 2 0 0 1-1.87-1.87C2 19.063 2 18.988 2 18.84l.001-.127a2 2 0 0 1 .52-1.284l.088-.092l1.744-1.8M9.5 18.5h5"/><path fill="currentColor" d="M12.75 6.75a.75.75 0 1 1-1.5 0a.75.75 0 0 1 1.5 0"/></g></svg>
                 <p class="text-sm">Full suite of digital & technology solutions to grow your business</p>
@@ -322,7 +322,7 @@
           <!-- End Title -->
 
           <!-- Grid -->
-          <div class="grid grid-cols-3 gap-5 lg:items-center">
+          <div class="grid md:grid-cols-3 gap-5 lg:items-center">
             <div class="card w-full rounded-xl shadow border flex flex-col items-center p-6" v-for="service in services" :key="service">
               <Icon :name='service.icon' class="w-8 h-8 text-cyan-600" />
               <h2 class="text-xl font-bold mt-4 text-center">{{ service.title }}</h2>
@@ -335,12 +335,12 @@
       <!-- End Approach -->
 
       <!-- Key Tech Expertise -->
-       <section class="w-full bg-cyan-900 py-20">
+       <section class="w-full bg-cyan-900 py-10 md:py-20 px-5 md:px-0">
         <div class="max-w-5xl mx-auto">
           <h2 class="text-4xl font-bold text-white mb-1">Key Tech Expertise</h2>
           <p class="text-gray-100">We analyze your product concept and business specifics, and bind these conclusions with industry standards, market trends, and appropriate tech stacks. Our software development firm builds top products for a particular industry, namely:</p>
         </div>
-        <div class="grid grid-cols-5 gap-4 max-w-5xl mx-auto mt-12">
+        <div class="grid grid-cols-2 md:grid-cols-5 gap-4 max-w-5xl mx-auto mt-12">
           <button class="flex flex-col items-center bg-white rounded-2xl py-5">
             <Icon name="mdi:apple-ios" class="w-20 h-20" />
             <span>iOS</span>
@@ -431,9 +431,6 @@
                 </div>
 
                 <div class="flex gap-4">
-                  <svg class="flex-shrink-0 size-5 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-                  </svg>
 
                   <div class="grow hidden">
                     <p class="text-sm text-gray-600 dark:text-neutral-400">
@@ -490,25 +487,8 @@
 
           <!-- Social Brands -->
           <div class="text-center md:text-end space-x-2">
-            <a class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-black hover:text-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:text-neutral-400" href="#">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-              </svg>
-            </a>
-            <a class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-black hover:text-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:text-neutral-400" href="#">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-              </svg>
-            </a>
-            <a class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-black hover:text-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:text-neutral-400" href="#">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-              </svg>
-            </a>
-            <a class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-black hover:text-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:text-neutral-400" href="#">
-              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M3.362 10.11c0 .926-.756 1.681-1.681 1.681S0 11.036 0 10.111C0 9.186.756 8.43 1.68 8.43h1.682v1.68zm.846 0c0-.924.756-1.68 1.681-1.68s1.681.756 1.681 1.68v4.21c0 .924-.756 1.68-1.68 1.68a1.685 1.685 0 0 1-1.682-1.68v-4.21zM5.89 3.362c-.926 0-1.682-.756-1.682-1.681S4.964 0 5.89 0s1.68.756 1.68 1.68v1.682H5.89zm0 .846c.924 0 1.68.756 1.68 1.681S6.814 7.57 5.89 7.57H1.68C.757 7.57 0 6.814 0 5.89c0-.926.756-1.682 1.68-1.682h4.21zm6.749 1.682c0-.926.755-1.682 1.68-1.682.925 0 1.681.756 1.681 1.681s-.756 1.681-1.68 1.681h-1.681V5.89zm-.848 0c0 .924-.755 1.68-1.68 1.68A1.685 1.685 0 0 1 8.43 5.89V1.68C8.43.757 9.186 0 10.11 0c.926 0 1.681.756 1.681 1.68v4.21zm-1.681 6.748c.926 0 1.682.756 1.682 1.681S11.036 16 10.11 16s-1.681-.756-1.681-1.68v-1.682h1.68zm0-.847c-.924 0-1.68-.755-1.68-1.68 0-.925.756-1.681 1.68-1.681h4.21c.924 0 1.68.756 1.68 1.68 0 .926-.756 1.681-1.68 1.681h-4.21z" />
-              </svg>
+            <a class="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-black hover:text-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:text-neutral-400" href="https://www.instagram.com/meetouinnovation/" target="_blank">
+              <Icon name="hugeicons:instagram" class="w-5 h-5" />
             </a>
           </div>
           <!-- End Social Brands -->
